dc789fda1a8af4cfadde8f9a39d9aca55a448a15
MinPro <mbs0722@gmail.com>
2024-11-03 19:37:43 -0500

    Update student_code.py

--- student_code.py
+++ student_code.py
@@ -1,7 +1,6 @@
 import random as rnd
 
 def decrypt(C):
-  M=""
   #entrez votre code ici.
   #Vous pouvez créer des fonctions auxiliaires et adapter le code à votre façon mais decrypt dois renvoyer le message décrypté
 
