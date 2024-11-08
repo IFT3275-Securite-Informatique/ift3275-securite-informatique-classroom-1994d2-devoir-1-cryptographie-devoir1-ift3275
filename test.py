e77fda5425bb8cddb3c315d1977a4265922ecc82
loccha <charlotte.locas@hotmail.com>
2024-11-08 16:35:57 -0500

    test_additionnels et ratios

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import random
 import time
 import unittest
 from difflib import SequenceMatcher, unified_diff
-from crypt import *
-from student_code import decrypt  # Remplacer par le nom de la fonction de déchiffrement
+from crypt import * # type: ignore
+from student_code import decrypt, dict_cryptogramme  # Remplacer par le nom de la fonction de déchiffrement
 
 
 def similarity_ratio(str1, str2):
@@ -49,6 +49,7 @@ class TestDecryption(unittest.TestCase):
         symboles = ['b', 'j', '\r', 'J', '”', ')', 'Â', 'É', 'ê', '5', 't', '9', 'Y', '%', 'N', 'B', 'V', '\ufeff', 'Ê', '?', '’', 'i', ':', 's', 'C', 'â', 'ï', 'W', 'y', 'p', 'D', '—', '«', 'º', 'A', '3', 'n', '0', 'q', '4', 'e', 'T', 'È', '$', 'U', 'v', '»', 'l', 'P', 'X', 'Z', 'À', 'ç', 'u', '…', 'î', 'L', 'k', 'E', 'R', '2', '_', '8', 'é', 'O', 'Î', '‘', 'a', 'F', 'H', 'c', '[', '(', "'", 'è', 'I', '/', '!', ' ', '°', 'S', '•', '#', 'x', 'à', 'g', '*', 'Q', 'w', '1', 'û', '7', 'G', 'm', '™', 'K', 'z', '\n', 'o', 'ù', ',', 'r', ']', '.', 'M', 'Ç', '“', 'h', '-', 'f', 'ë', '6', ';', 'd', 'ô', 'e ', 's ', 't ', 'es', ' d', '\r\n', 'en', 'qu', ' l', 're', ' p', 'de', 'le', 'nt', 'on', ' c', ', ', ' e', 'ou', ' q', ' s', 'n ', 'ue', 'an', 'te', ' a', 'ai', 'se', 'it', 'me', 'is', 'oi', 'r ', 'er', ' m', 'ce', 'ne', 'et', 'in', 'ns', ' n', 'ur', 'i ', 'a ', 'eu', 'co', 'tr', 'la', 'ar', 'ie', 'ui', 'us', 'ut', 'il', ' t', 'pa', 'au', 'el', 'ti', 'st', 'un', 'em', 'ra', 'e,', 'so', 'or', 'l ', ' f', 'll', 'nd', ' j', 'si', 'ir', 'e\r', 'ss', 'u ', 'po', 'ro', 'ri', 'pr', 's,', 'ma', ' v', ' i', 'di', ' r', 'vo', 'pe', 'to', 'ch', '. ', 've', 'nc', 'om', ' o', 'je', 'no', 'rt', 'à ', 'lu', "'e", 'mo', 'ta', 'as', 'at', 'io', 's\r', 'sa', "u'", 'av', 'os', ' à', ' u', "l'", "'a", 'rs', 'pl', 'é ', '; ', 'ho', 'té', 'ét', 'fa', 'da', 'li', 'su', 't\r', 'ée', 'ré', 'dé', 'ec', 'nn', 'mm', "'i", 'ca', 'uv', '\n\r', 'id', ' b', 'ni', 'bl']
         nb_symboles = len(symboles)
         dictionnaire = gen_key(symboles)
+        #print("arpad dictionnaire", dictionnaire)
 
         random.seed(time.time())
 
@@ -71,6 +72,21 @@ class TestDecryption(unittest.TestCase):
         # Appeler la fonction de déchiffrement de l'étudiant
         decrypted_message = decrypt(cryptogram)
 
+        #Pour les tests
+        dictionnaire_cryptogramme = {valeur: key for key, valeur in dict_cryptogramme().items()}
+
+        #fonction de comparaison
+        caractere_trouve = []
+        for key in dictionnaire:
+          #  print("key: ", key)
+          #  print("dictionnaire.get(key): ", dictionnaire.get(key))
+          #  print("dictionnaire_cryptogramme.get(key): ", dictionnaire_cryptogramme.get(key))
+            if(dictionnaire.get(key) == dictionnaire_cryptogramme.get(key)):
+                caractere_trouve.append(key)
+        print("liste de caracteres trouvés: ", caractere_trouve)
+            
+
+
         # Calculer la similarité
         similarity = similarity_ratio(original_message, decrypted_message)
         print(f"Similarité : {similarity:.2%}")
@@ -78,7 +94,7 @@ class TestDecryption(unittest.TestCase):
         # Imprimer les différences si la similarité est inférieure à 99.5 %
         if similarity < 0.995:
             print("Différences entre les messages :")
-            print_diff(original_message, decrypted_message)
+            #print_diff(original_message, decrypted_message)
 
         # Vérifier que la similarité entre le message original et le message déchiffré est d'au moins 99.5 %
         self.assertGreaterEqual(similarity, 0.995,
